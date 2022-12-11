01d67d8e94b671b75ba11b4a1d0b4b11f6c8c3e3
RabbitSecurity <bostjan@DESKTOP-QU9QRHC.localdomain>
2022-12-11 16:27:17 +0100

    Urejanje maila 1. del

--- config.php
+++ config.php
@@ -19,6 +19,8 @@ define("Sklepnik_SMTP_Encryption", 'starttls');
 define('Sklepnik_SMTP_Username', '**************');
 define('Sklepnik_SMTP_Password', '**************');
 
+//Super kurac
+
 define('stencas_password', '**************');
 
 //Generiranje tokenov (naj ostane skrivnost)
